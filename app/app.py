fb69a16a3343257e9571d396eb95e75cb1b4551a
kaitoohsiro <kaitooshiro7@gmail.com>
2022-06-21 12:22:41 +0900

    dockerの環境構築

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import voiceRecognition as vr
+import voiceTextualization as vt
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "index page"
+
+@app.route("/vr")
+def voiceRecognition():
+    return vr.voice_r_module()
+
+@app.route("/vt")
+def voiceTextualization():
+    return vt.voice_t_module()
