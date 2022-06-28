8bdf9e7a40109ccea500beba563410fdd87973a6
kaitoohsiro <kaitooshiro7@gmail.com>
2022-06-28 19:42:45 +0900

    voice_text完成 flask docker環境作成

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import asyncio
 import voiceRecognition as vr
 import voiceTextualization as vt
 
@@ -10,9 +11,22 @@ def index():
 
 @app.route("/vr")
 def voiceRecognition():
-    return vr.voice_r_module()
+    # emotionAnalyzers = vr.SentimentAnalysis()
+    # emotionAnalyzers.read_text("こんにちは！今日は楽しい日になりそうですね!")
+    # result = emotionAnalyzers.analyze()
+    return "result"
 
 @app.route("/vt")
 def voiceTextualization():
     return vt.voice_t_module()
 
+@app.route("/test")
+def voiceEAPI():
+    text = vt.voice_t_module()
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(vr.voice_r_module(text))
+    # result = vr.voice_r_module(text)
+    # emotionAnalyzers.read_text(text)
+    # loop = asyncio.get_event_loop()
+    # result = loop.run_until_complete(emotionAnalyzers.analyze(loop=loop))
+    return result
